8f210922e85c87b1b987f99ab69048a50b1341df
danial.asaria <dasaria@usc.edu>
2022-05-06 00:04:28 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "*.html"
+  ],
+  mode: "jit",
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Fira sans", "sans-serif", "sans-serif"]
+    }
+  },
+  plugins: [],
+}
